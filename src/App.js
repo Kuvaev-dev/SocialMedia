aa87623a49d17cce1927e18e03ed57c4e4945996
Kuvaev-dev <likeaboss13371@gmail.com>
2024-09-15 15:13:14 +0300

    Main Components Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import "./App.css";
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Home, Profile, Register, Login, ResetPassword } from "./pages";
+
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
 
 function App() {
   return (
     <div className="w-full min-h-[100vh]">
-      
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
+        </Route>
+
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+      </Routes>
     </div>
   );
 }
