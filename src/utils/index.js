31322990cfe962e72d362b886a4d1200b77d01c3
Kuvaev-dev <likeaboss13371@gmail.com>
2024-11-02 20:51:30 +0200

    Build Config Added

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { SetPosts } from "../redux/postSlice";
 
-const API_URL = "http://localhost:8800";
+const API_URL = "https://kshareserver.onrender.com";
+// "http://localhost:8800";
 
 export const API = axios.create({
   baseURL: API_URL,
